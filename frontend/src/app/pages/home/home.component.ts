7476d76ad2d4515db56d54d9cbf084e6f5a2e4f8
Pedro Jorge <skitvandarken2013@gmail.com>
2025-03-26 14:24:37 +0100

    mvp email funciona

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../layout/menu/menu.component';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MenuComponent
+    MenuComponent, 
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
+
+
 export class HomeComponent {
+
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_3nlw88s', 'template_jd3jtkp', e.target as HTMLFormElement, {
+        publicKey: '9HFwqoPfqWMMnzdk8',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 
 }
